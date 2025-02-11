471910b36ee42520572078c4928b1e92803c5b73
Adarsh Kumar <adarshkumar712.ak@gmail.com>
2025-02-12 01:55:16 +0530

    Missed this file

--- memory_builder/app/memory/memory_layers/basic_layer.py
+++ memory_builder/app/memory/memory_layers/basic_layer.py
@@ -4,8 +4,14 @@ class BasicLayer(MemoryLayer):
     def __init__(self, config):
         super().__init__("BasicLayer", config)
 
-    def ingest(self, data):
-        pass
+    def convert_to_embedding(self, data):
+        data = self.chunker(data)
+        return [self.embedding_client(text) for text in data]
 
-    def retrieve(self, query):
-        pass
+    def ingest(self, data):
+        data_embedding = self.convert_to_embedding(data)
+        self.retriever.ingest(data_embedding)
+
+    def retrieve(self, query) -> list:
+        query_embedding = self.embedding_client(query)
+        return self.retriever.retrieve(query_embedding)
